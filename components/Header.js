e5c33107246c406add85c3a8fb1bbd7f439c3497
YongYuH <larry830326@hotmail.com>
2017-11-23 01:40:06 +0800

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <button>hamburger bar</button>
+    <button>logo</button>
+    <button>log in</button>
+  </div>
+);
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Index = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default Index;
